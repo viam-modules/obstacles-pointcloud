76e207cfcc89f28ef2f5e39deeb412fd81d3968e
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-18 15:16:14 -0400

    init module with template

---
+++ cmd/module/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"obstaclespointcloud"
+	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/services/vision"
+)
+
+func main() {
+	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
+	module.ModularMain(resource.APIModel{ vision.API, obstaclespointcloud.ObstaclesPointcloud})
+}
