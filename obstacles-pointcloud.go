f948a528abc8bad8cb101eac20b08f888b186584
Grant Mulitz <gamulitz@gmail.com>
2025-12-17 13:49:57 -0500

    add ground normal to config

--- obstacles-pointcloud.go
+++ obstacles-pointcloud.go
@@ -32,14 +32,21 @@ func init() {
 	})
 }
 
+type NormalVec struct {
+	X float64 `json:"x,omitempty"`
+	Y float64 `json:"y,omitempty"`
+	Z float64 `json:"z,omitempty"`
+}
+
 type ObstaclesPointCloudConfig struct {
-	MinPtsInPlane        int     `json:"min_points_in_plane"`
-	MinPtsInSegment      int     `json:"min_points_in_segment"`
-	MaxDistFromPlane     float64 `json:"max_dist_from_plane_mm"`
-	ClusteringRadius     int     `json:"clustering_radius"`
-	ClusteringStrictness float64 `json:"clustering_strictness"`
-	AngleTolerance       float64 `json:"ground_angle_tolerance_degs"`
-	DefaultCamera        string  `json:"camera_name"`
+	MinPtsInPlane        int       `json:"min_points_in_plane"`
+	MinPtsInSegment      int       `json:"min_points_in_segment"`
+	MaxDistFromPlane     float64   `json:"max_dist_from_plane_mm"`
+	ClusteringRadius     int       `json:"clustering_radius"`
+	ClusteringStrictness float64   `json:"clustering_strictness"`
+	AngleTolerance       float64   `json:"ground_angle_tolerance_degs"`
+	DefaultCamera        string    `json:"camera_name"`
+	GroundPlaneNormalVec NormalVec `json:"ground_plane_normal_vec"`
 }
 
 func (cfg *ObstaclesPointCloudConfig) Validate(path string) ([]string, []string, error) {
@@ -90,12 +97,19 @@ func registerPointCloudSegmenter(
 	if conf == nil {
 		return nil, errors.New("config for obstacles pointcloud segmenter cannot be nil")
 	}
+
+	var groundPlaneNormalVec r3.Vector
+	if conf.GroundPlaneNormalVec.X == 0 && conf.GroundPlaneNormalVec.Y == 0 && conf.GroundPlaneNormalVec.Z == 0 {
+		groundPlaneNormalVec = r3.Vector{X: 0, Y: 0, Z: 1}
+	} else {
+		groundPlaneNormalVec = r3.Vector{X: conf.GroundPlaneNormalVec.X, Y: conf.GroundPlaneNormalVec.Y, Z: conf.GroundPlaneNormalVec.Z}
+	}
 	// build the clustering config
 	cfg := &ErCCLConfig{
 		MinPtsInPlane:        conf.MinPtsInPlane,
 		MinPtsInSegment:      conf.MinPtsInSegment,
 		MaxDistFromPlane:     conf.MaxDistFromPlane,
-		NormalVec:            r3.Vector{0, -1, 0},
+		NormalVec:            groundPlaneNormalVec,
 		AngleTolerance:       conf.AngleTolerance,
 		ClusteringRadius:     conf.ClusteringRadius,
 		ClusteringStrictness: conf.ClusteringStrictness,
